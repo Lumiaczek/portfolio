c36dd4fb1ac40a5e252c802e818d355bcde0a321
Daniel Borowski <danielborowski05@gmail.com>
2023-06-01 12:28:25 +0200

    Certyfikaty -v2

--- src/lib/about/Crt.svelte
+++ src/lib/about/Crt.svelte
@@ -1,14 +1,14 @@
 <div class="space-y-3">
 	<div class="rounded-xl flex flex-row items-center bg-opacity-25 bg-blue-700 p-4">
 		<img src="/CCNAITN__1_.png " alt="" width="100" />
-		<div class="flex flex-col ml-3">
+		<div class="flex flex-col ml-3 text-left">
 			<p class="text-xl font-medium">CCNA: Introduction to Networks</p>
 			<p class="text-sm">Cisco Networking Academy</p>
 		</div>
 	</div>
 	<div class="rounded-xl flex flex-row items-center bg-opacity-25 bg-blue-700 p-4">
 		<img src="/I2CS__1_.png" alt="" width="100" />
-		<div class="flex flex-col ml-3">
+		<div class="flex flex-col text-left ml-3">
 			<p class="text-xl font-medium">Introduction to Cybersecurity</p>
 			<p class="text-sm">Cisco Networking Academy</p>
 		</div>
